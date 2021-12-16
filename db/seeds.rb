80b29d727a2a7d61ef983c6d8784469edd82486c
siva <siva@magiccrate.in>
2021-12-16 18:19:19 +0530

    message feature setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Message.create(body: 'Hello',user_id: 1)
+Message.create(body: 'Hello test',user_id: 1)
+Message.create(body: 'Hello testing',user_id: 1)
